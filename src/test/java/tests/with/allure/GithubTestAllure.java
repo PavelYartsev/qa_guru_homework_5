0ad2b3be05c57d907925110bbb2fd41f33711ee1
Pavel <iartsev.pavel@gmail.com>
2021-04-13 22:37:54 +0300

    New Test class added build.gradle small changes

--- src/test/java/tests/with/allure/GithubTestAllure.java
+++ src/test/java/tests/with/allure/GithubTestAllure.java
@@ -3,20 +3,25 @@ package tests.with.allure;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import io.qameta.allure.Allure;
 
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
-
-public class GitHubAllureTest {
+public class GithubTestAllure {
 
     private final static String siteUrl = "https://github.com/";
     private final static String repository = "eroshenkoam/allure-example";
     private final static int issueNumber = 68;
 
     @Test
-    public void searchForIssue(){
-        open(siteUrl);
+    public void searchForIssue() {
+        step("Step 1", () -> {
+            open(siteUrl);
+        });
+
         $(".input-sm").click();
         $(".input-sm").sendKeys(repository);
         $(".input-sm").submit();
